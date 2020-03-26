8679d00a1642ade945384624331499bdb3674190
Vivek Nagaraj <vnaga08@gmail.com>
2020-03-26 07:43:14 -0500

    Created a data processing class to manage covid-19 datasets. Started some simple data mining inquirires.

--- covid_19/pull_covid_data.sh
+++ covid_19/pull_covid_data.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd data
-git pull https://github.com/CSSEGISandData/COVID-19.git
+if [ -d "COVID-19" ]; then
+  git pull 
+else
+  git clone https://github.com/CSSEGISandData/COVID-19.git
+fi
